b678aa71d7236324bb78d8251f492e8d1bb27459
Juan Francisco <contrerasjf.0@hotmail.com>
2021-03-02 20:40:31 -0600

    test 1

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+
+router = express.Router();
+
+router.get('/api', function(req, res) {
+  res.send('I want a beer');
+});
+
+app.use(cors());
+
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('The server is running on the port: ', 3000);
+});
+
+module.exports = app;
